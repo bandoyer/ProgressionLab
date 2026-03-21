dc2b1741534d844495bc6dc2f966b79382c7c549
Dan Boyer <bandoyer@gmail.com>
2026-03-21 02:37:21 -0400

    Add RPE value object and unit tests to enforce validation

---
+++ src/ProgressionLab.Core/ValueObjects/RateOfPerceivedExertion.cs
@@ -0,0 +1,17 @@
+﻿namespace ProgressionLab.Core.ValueObjects;
+
+public readonly record struct RateOfPerceivedExertion 
+{
+  public RateOfPerceivedExertion(decimal value)
+  {
+    if (value < 1m)
+      throw new ArgumentOutOfRangeException(nameof(value), "RPE must be greater than or equal to 1.");
+    if (value > 10m)
+      throw new ArgumentOutOfRangeException(nameof(value), "RPE must be less than or equal to 10.");
+    if (value % 0.5m != 0m)
+      throw new ArgumentOutOfRangeException(nameof(value), "RPE must be divisible by 0.5.");
+    Value = value;
+  }
+
+  public decimal Value { get; }
+}
---
+++ tests/ProgressionLab.UnitTests/Core/ValueObjects/RpeTests.cs
@@ -0,0 +1,46 @@
+﻿using ProgressionLab.Core.ValueObjects;
+
+namespace ProgressionLab.UnitTests.Core.ValueObjects;
+
+public class RpeTests
+{
+  [Fact]
+  public void CanCreateRpeOfTen()
+  {
+    var rpe10 = new RateOfPerceivedExertion(10);
+    rpe10.Value.ShouldBe(10);
+  }
+
+  [Fact]
+  public void CanCreateRpeOfNinePointFive()
+  {
+    var rpe95 = new RateOfPerceivedExertion(9.5m);
+    rpe95.Value.ShouldBe(9.5m);
+  }
+
+  [Fact]
+  public void CannotCreateRpeGreaterThanTen()
+  {
+    Should.Throw<ArgumentOutOfRangeException>(() => new RateOfPerceivedExertion(11));
+  }
+  
+  [Fact]
+  public void CannotCreateRpeLessThanOne()
+  {
+    Should.Throw<ArgumentOutOfRangeException>(() => new RateOfPerceivedExertion(0));
+  }
+
+  [Fact]
+  public void CannotCreateRpeThatIsNotDivisibleByHalf()
+  {
+    Should.Throw<ArgumentOutOfRangeException>(() => new RateOfPerceivedExertion(9.7m));
+  }
+  
+  [Fact]
+  public void TwoRpesAreEqual()
+  {
+    var rpe10 = new RateOfPerceivedExertion(10);
+    var rpe10Again = new RateOfPerceivedExertion(10);
+    rpe10.ShouldBe(rpe10Again);
+  }
+}
